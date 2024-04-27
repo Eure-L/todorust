f98d2ecd1d8a8415b15f06d66b0c8dae73c08285
vjeunehomme <eurel.vj@gmail.com>
2024-04-27 22:11:12 +0200

    feat: basic style

--- src/routes.rs
+++ src/routes.rs
@@ -1,9 +1,20 @@
-use crate::api::{add_task_handle, delete_task_handle, index, set_task_status_handle};
-use axum::routing::{delete, get, post, put};
+use std::path::PathBuf;
+
 use axum::Router;
+use axum::routing::{delete, get, post, put};
+use tower_http::services::ServeDir;
+
+use crate::api::{add_task_handle, delete_task_handle, index, set_task_status_handle};
 
 pub fn get_routes() -> Router {
+
+    let static_dir = PathBuf::from(env!("CARGO_MANIFEST_DIR")).join("static");
+
     Router::new()
+        .nest_service(
+            "/static",
+            ServeDir::new(static_dir),
+        )
         .route("/", get(index))
         .route("/tasks/add", post(add_task_handle))
         .route("/tasks/delete", delete(delete_task_handle))
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,18 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js,rs}"],
+  mode: 'jit',
+  content: ["./templates/**/*.{html,js,rs}"],
+  darkMode: 'media', // or 'class'
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#6C0345",
+        secondary: "#DC6B19",
+        tierciary: "#F7C566",
+        fourthiary: "#FFF8DC"
+      }
+
+    },
   },
   plugins: [],
-
 }
